7b2ab743e1fa22e8a8987e627c6b8761d378ba62
Tobias Koppers <tobias.koppers@googlemail.com>
2024-09-04 09:23:43 +0200

    add new backend feature

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -1373,6 +1373,7 @@ export default async function build(
               config.experimental?.turbo?.root ||
               config.outputFileTracingRoot ||
               dir,
+            distDir,
             nextConfig: config,
             jsConfig: await getTurbopackJsConfig(dir, config),
             watch: {
