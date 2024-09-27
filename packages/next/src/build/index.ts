3d415830d807369e7644bbc6888b54ae853409bd
Tobias Koppers <tobias.koppers@googlemail.com>
2024-09-27 14:51:14 +0200

    increase concurrectly over time

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -1481,7 +1481,8 @@ export default async function build(
           'Building'
         )
         const promises: Promise<any>[] = []
-        const sema = new Sema(10)
+        const sema = new Sema(5)
+        let extraSema = 5
         const enqueue = (fn: () => Promise<void>) => {
           promises.push(
             (async () => {
@@ -1490,6 +1491,10 @@ export default async function build(
                 await fn()
               } finally {
                 sema.release()
+                if (extraSema > 0) {
+                  extraSema--
+                  sema.release()
+                }
                 progress()
               }
             })()
